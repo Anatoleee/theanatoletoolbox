3fbff884d086e4169fefa395ac78998c5b7d4164
Anatole <aatole.capelle@gmail.com>
2024-06-05 22:38:18 +0200

    updated wikit.js

--- WikitModifier/generated/wikit.js
+++ WikitModifier/generated/wikit.js
@@ -1,7 +1,7 @@
 
-/* Made By : Anatole Capelle (github.com/Anatoleee) W/ PhC */
-/* This is  a fake template (i do not have the right to publish the real version of this file) */
-<script type="text/javascript" src="https:/*censored*/"></script>
+/* Made By : Anatole Capelle (github.com/Anatoleee) W/ PhC
+ This is  a fake template (i do not have the right to publish the real version of this file)
+       <script type="text/javascript" src="https:censored"></script>
 <script type="text/javascript">
     wrapWebchat({
         },
@@ -20,4 +20,4 @@
            minimizeIconDescription: 'sdfs',
        },
     });
-</script>
+</script>*/
